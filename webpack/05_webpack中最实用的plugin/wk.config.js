c7b374bbe572bbbadb3ecc1c88b04d234f601d9e
yangfu <yffto521@163.com>
2022-10-26 00:25:32 +0800

    简单的plugin配置

---
+++ webpack/05_webpack中最实用的plugin/wk.config.js
@@ -0,0 +1,94 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { DefinePlugin } = require('webpack')
+const CopyWebpckPlugin = require('copy-webpack-plugin')
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    filename: "bundle.js",
+    // 必须是一个绝对路径
+    path: path.resolve(__dirname, "./build"),
+    // assetModuleFilename: './img/[name].[hash:6][ext]'
+  },
+  module: {
+    rules: [
+      {
+        // 规则使用正则表达式
+        test: /\.css$/, // 匹配资源
+        use: [
+          // { loader: "css-loader" },
+          // 注意: 编写顺序(从下往上, 从右往做, 从后往前)
+          "style-loader", 
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1
+            }
+          },
+          "postcss-loader"
+        ],
+        // loader: "css-loader"
+      },
+      {
+        test: /\.less$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2
+            }
+          },
+          "postcss-loader",
+          "less-loader"
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        // type: 'asset/resource file-loader 的效果
+        // type: 'asset/inline' usr-loader 的效果
+        type: 'asset', 
+        generator: {
+          filename: 'img/[name].[hash:6][ext]'
+        },
+        parser: {
+          dataUrlCondition: {
+            maxSize: 1024 * 100
+          }
+        }
+      },
+      {
+        test: /\.eot|ttf|woff2?$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'font/[name].[hash:6][ext]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'code html',
+      template: './public/index.html'
+    }),
+    new DefinePlugin({
+      BASE_URL: '"./"'
+    }),
+    new CopyWebpckPlugin({
+      patterns: [
+        {
+          from: 'public',
+          globOptions: {
+            ignore: [
+              '**/*.html',
+              '**/*.txt',
+              '**/*/DS_Store'
+            ]
+          }
+        }
+      ]
+    })
+  ]
+}
